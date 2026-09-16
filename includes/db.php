aea30361faa63048357e7c88ef73a5ad7feaf5da
Opio Lamecky <opio12@gmail.com>
2026-09-16 14:30:12 +0300

    Opio Lameck

--- includes/db.php
+++ includes/db.php
@@ -5,10 +5,11 @@ $host = getenv('DB_HOST') ?: 'localhost';
 $dbname = getenv('DB_NAME') ?: 'postgres';
 $username = getenv('DB_USER') ?: 'postgres';
 $password = getenv('DB_PASS') ?: '';
+$port = getenv('DB_PORT') ?: 5432;
 
 try {
     $pdo = new PDO(
-        "pgsql:host=$host;port=5432;dbname=$dbname;user=$username;password=$password"
+        "pgsql:host=$host;port=$port;dbname=$dbname;user=$username;password=$password;sslmode=require"
     );
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
